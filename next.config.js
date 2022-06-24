a0f0fb813c0b540f620d3838dbd6104500ed8609
Tom Chen <developer@tomchentw.com>
2022-06-24 16:38:41 +0800

    chore(package.json): add @svgr/webpack to devDependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.js$/],
+      },
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
